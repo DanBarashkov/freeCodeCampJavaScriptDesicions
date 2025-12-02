9b069dc1c3cc2c3ea5103defafb17d298c7f58fa
Danil Barashkov <volbard49@gmail.com>
2025-12-02 10:51:44 +0300

    Update_2025-12-02

---
+++ 00_Learning/leap_year.js
@@ -0,0 +1,16 @@
+
+function isLeapYear(year) {
+  if(year % 4 == 0 && year % 400 == 0) {
+    return `${year} is a leap year.`
+  } else if (year % 4 == 0 && year % 100 == 0) {
+    return `${year} is not a leap year.`
+  } else if(year % 4 == 0 && year % 100 != 0) {
+    return `${year} is a leap year.`
+  } else {
+    return `${year} is not a leap year.`
+  }
+}
+
+let year = 1200;
+let result = isLeapYear(year);
+console.log(result);
